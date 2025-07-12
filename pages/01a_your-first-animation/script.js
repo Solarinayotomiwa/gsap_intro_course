e35776f6d885d02890af53967933b5f60a6bd93b
Solarinayotomiwa <61776239+Solarinayotomiwa@users.noreply.github.com>
2025-07-12 04:01:08 +0100

    first commit

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,14 @@
+gsap.to('.card', {
+    opacity: 1,
+    scale: 1,
+    duration: 1,
+    rotate: 90,
+    onComplete: () => {
+        gsap.to('.card', {
+            y:-10,
+            repeat: -1,
+            yoyo: true,
+            duration: 0.5
+        })
+    }
+});
